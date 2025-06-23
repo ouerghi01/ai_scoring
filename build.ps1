c1ff021b4bf9bdeeb97c84975e8c0b995b929f18
ouerghi01 <mohamedaziz.ouerghi@etudiant-enit.utm.tn>
2025-06-23 17:25:56 +0100

    add changes ..x.

--- build.ps1
+++ build.ps1
@@ -5,15 +5,11 @@ $imageName = "flask-docker-app"
 $tarFile = "flask-app.tar"
 
 # Step 1: Build the Docker image
-Write-Host "🔨 Building Docker image..."
 docker build -t $imageName .
 
 # Step 2: Run the Docker container
-Write-Host "🚀 Running Docker container..."
 docker run -d -p 5000:5000 $imageName
 
 # Step 3: Save the Docker image to a tar file
-Write-Host "💾 Saving Docker image to tar file..."
 docker save -o $tarFile $imageName
 
-Write-Host "✅ Done! Image saved as $tarFile"
